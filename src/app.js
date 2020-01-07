731b2317ad0f6c80226928ab57d0415abc51ce9f
carolbrandao <bcarol744@gmail.com>
2020-01-07 15:03:32 +0100

    Initial setup

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './app.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          File Viewer.
+        </p>
+      </header>
+    </div>
+  )
+}
+
+export default App
