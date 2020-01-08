dd82d0eaf54eba7aecb29294c1eff8e98f2ca09e
carolbrandao <bcarol744@gmail.com>
2020-01-08 22:13:02 +0100

    Add unit tests for getEditedArray function

--- src/file-viewer.test.js
+++ src/file-viewer.test.js
@@ -38,13 +38,41 @@ describe('getArrayAfterDeletion', () => {
 
   it('item to be deleted is a folder', () => {
     // arrange
-    const array = ['a/b/c','a/b', 'a/c', 'd']
-    const element = 'a'
+    const array = ['a/b/c','a/b/c/a/b', 'a/c', 'd/a/b/c']
+    const element = 'a/b'
 
     //act
     const newArray = getArrayAfterDeletion(element, array)
 
     // assert
-    expect(newArray).toEqual(['d'])
+    expect(newArray).toEqual(['a/c','d/a/b/c'])
+  })
+})
+
+describe('getEditedArray', () => {
+  it('item to be idited is a single file', () => {
+    // arrange
+    const array = ['a/b/c','a/b', 'a/c']
+    const element = 'a/b/c'
+    const newPath = 'a/b/test'
+
+    //act
+    const newArray = getEditedArray(element, newPath, array)
+
+    // assert
+    expect(newArray).toEqual(['a/b', 'a/c', 'a/b/test'])
+  })
+
+  it('item to be edited is a folder', () => {
+    // arrange
+    const array = ['a/b/c','a/b/c/a/b', 'a/c', 'd/a/b/c']
+    const element = 'a/b'
+    const newPath = 'a/test'
+
+    //act
+    const newArray = getEditedArray(element, newPath, array)
+
+    // assert
+    expect(newArray).toEqual(['a/test/c','a/test/c/a/b', 'a/c', 'd/a/b/c'])
   })
 })
